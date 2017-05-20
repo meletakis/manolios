5483e3628003449377e5bdc5fa551488431ff937
Giannis Meletakis <giannis@dell.zte.com.cn>
2017-05-20 14:09:17 +0300

    Generate Team model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170520104849) do
+ActiveRecord::Schema.define(version: 20170520110814) do
+
+  create_table "teams", force: :cascade do |t|
+    t.string   "name"
+    t.decimal  "balance"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
