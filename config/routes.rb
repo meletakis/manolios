93ff5b9be2b655be5f9e11b5b52316dcf80a2dac
Giorgos Batistatos <batistatos@workable.com>
2017-05-20 15:14:48 +0300

    Set up Devise for user registration and authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users,
+    path_names: {
+      sign_in: "login",
+      sign_out: "logout"
+    }
+
   root 'welcome#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
