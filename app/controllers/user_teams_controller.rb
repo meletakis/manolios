946b82c050049f6b02980afddabd58cafba0b854
Giannis Meletakis <meletakis@gmail.com>
2017-05-20 20:10:48 +0300

    Show Team, Team balance, UserTeam Balance in user_teams index

--- app/controllers/user_teams_controller.rb
+++ app/controllers/user_teams_controller.rb
@@ -1,4 +1,8 @@
 class UserTeamsController < ApplicationController
+  before_action :authenticate_user!
+
   def index
+    @teams = User.find(current_user.id).teams
+    @user_team_balances = UserTeamBalance.find(current_user.id)
   end
 end
