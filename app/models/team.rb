18e574c365f5af10735485aa6e5f54d3253127e6
Giannis Meletakis <meletakis@gmail.com>
2017-05-21 21:25:25 +0300

    Set init team balance to zero

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,5 +1,12 @@
 class Team < ActiveRecord::Base
+  before_create :default_balance
+
   has_many :user_team_balances
   has_many :users, through: :user_team_balances
   has_many :orders
+
+  private
+    def default_balance
+      self.balance = 0.0
+    end
 end
