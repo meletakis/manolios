20258510a58c83f833c46f0d841da0845c932a49
Giannis Meletakis <meletakis@gmail.com>
2017-05-21 21:12:47 +0300

    Create new team

--- app/controllers/team_controller.rb
+++ app/controllers/team_controller.rb
@@ -5,4 +5,21 @@ class TeamController < ApplicationController
     @teams = User.find(current_user.id).teams
     @user_team_balances = UserTeamBalance.find(current_user.id)
   end
+  def new
+    @team = Team.new
+  end
+
+  def create
+    @team = Team.new(team_params)
+    if @team.save
+      redirect_to @team
+    else
+      render "new"
+    end
+  end
+
+  private
+    def team_params
+      params.require(:team).permit(:name)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'team/index'
+
+  resources :team
 
   devise_for :users,
     path_names: {
