eb9e7bfa7b446a72a5bf417d1183ec1119fad614
Giannis Meletakis <meletakis@gmail.com>
2017-05-28 22:32:52 +0300

    Show order details

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,16 @@
 class OrdersController < ApplicationController
-	
+  helper_method :order_cost
 
+  def show
+  	current_order
+  end
+
+  private
+  	def current_order
+      @order = Order.find(params[:id])
+    end
+
+  	def order_cost(user)
+  		current_order.user_order_costs.find_by(user: user).cost
+  	end 
 end
