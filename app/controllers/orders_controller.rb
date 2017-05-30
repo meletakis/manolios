8830190ddebdf8120b1cded9abd0d6e02cfdfbbd
Giannis Meletakis <meletakis@gmail.com>
2017-05-30 21:00:49 +0300

    Add orders as team's nested resource

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,6 +5,46 @@ class OrdersController < ApplicationController
   	current_order
   end
 
+  def new
+    @order = Order.new
+    @team_users = current_team.users
+  end
+
+  def edit
+    current_team
+    all_users_except_current
+  end
+
+  def create
+    @order = Order.create(order_params)
+    if @order.save
+      redirect_to @order
+    else
+      render "new"
+    end
+  end
+
+  def update
+    current_team
+    if current_team.update(team_params)
+      current_team.user_team_balances.build(:user => current_user)
+      redirect_to @order
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+    current_team
+    @order.destroy
+    redirect_to root_path
+  end
+
+  private
+    def team_params
+      params.require(:team).permit(:name, user_ids: [])
+    end
+
   private
   	def current_order
       @order = Order.find(params[:id])
